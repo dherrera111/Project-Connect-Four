5d52e3b9d08623b684b5abe57fe9fba5f70c3180
VictorLeung91 <victorleung91@gmail.com>
2025-03-25 16:57:34 -0600

    Update Program.cs

--- Connect-Four/Program.cs
+++ Connect-Four/Program.cs
@@ -6,7 +6,24 @@
 
 
 using System;
+//Menu
+/***********************
+ * Game Menu
+ * 1. Human VS Human
+ * 2. Human VS AI
+ * 3. Exit
+ ********************/
 
+
+/*****************************Task 1****************************/
+/******Victor
+ * To create Menu with option 1,2,3 and exit to enter the game board
+*******/
+
+
+/******Dondon
+ * To create the game board
+ * ***********/
 namespace Connect_Four
 {
     internal class Program
