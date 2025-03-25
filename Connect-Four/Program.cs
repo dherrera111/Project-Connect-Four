85ebd9979a57137998e71429f61db951c1ca2ec9
dherrera111 <dondon.herrera@outlook.com>
2025-03-25 16:23:50 -0600

    Initial Commit

---
+++ Connect-Four/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Authors: Victor Leung and Dondon Herrera
+    SODV1202: Introduction to OOP
+    April 21, 2025
+*/
+
+
+using System;
+
+namespace Connect_Four
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Connect Four!");
+        }
+    }
+}
